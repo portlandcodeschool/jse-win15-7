dcfe38b0aefeb8d630cffc87f7ddcafcda1719bf
Robert Tribbia <rtribbia@gmail.com>
2015-02-18 17:34:39 -0800

    initial push

---
+++ rtribbia/template/memory-gui.js
@@ -0,0 +1,25 @@
+var MemoryGUI = (function () {
+
+	//...
+
+	function GuiCtor(container,game) {
+
+		// public instance methods:
+		this.reset = function() {
+			//...
+		}
+		this.show = function(where,what) {
+			//...
+		}
+		this.removeSoon = function(whereArr) {
+			//...
+		}
+		this.hideSoon = function(whereArr) {
+			//...
+		}
+
+		// Do some initial setup and rendering...
+	}
+
+	return GuiCtor;
+})();
