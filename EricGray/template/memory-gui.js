c8bfad1e3be08255dc2c223607519ae13a6d8085
Eric Gray <ericxgray@gmail.com>
2015-02-19 12:38:18 -0800

    CSS changes working so far, need to make some tweaks to hidden class

--- EricGray/template/memory-gui.js
+++ EricGray/template/memory-gui.js
@@ -3,18 +3,13 @@ var MemoryGUI = (function () {
 	//...
 
 	function GuiCtor(container, game) {
-		console.log('this is game: ')
-		console.log(game.size());
-		console.log('Is container a string?' + (typeof container === 'string'));
-		console.log('this is your container' + container);
-		console.log('this is your getElementById: ' + document.getElementById(container))
 		if(typeof container === 'string'){
 			container = document.getElementById(container);
 			console.log('this is now your container: ' + container);
 			};	
-	/*	if(!(container instanceof HTMLElement)) {
+		if(!(container instanceof HTMLElement)) {
 			return {}
-		}; */
+		}; 
 
 		this.el = function(){
 			if (!container){
@@ -28,6 +23,7 @@ var MemoryGUI = (function () {
 		// public instance methods:
 		this.reset = function() {
 			//...
+
 		}
 		this.show = function(where,what) {
 			//...
@@ -44,15 +40,38 @@ var MemoryGUI = (function () {
 */	
 
 		this.render = function(){
+			console.log('here is where your faceup is')
+			console.log(game1.faceupValue());
+			console.log('render shows this as game');
+			console.log(game);
 			container.innerHTML = '';
 			var playmat = document.createElement('table');
 			for (row = 0; row<2; row++){
 				var gameRow = document.createElement('tr');
-				gameRow.className = 'cardBorder';
+				//gameRow.className = 'cardBorder';
 				for (cell = 0; cell<(game.size()*0.5); cell++){
 					var cardCell = document.createElement('td');
-					cardCell.setAttribute('id', (2*row + cell));
-					cardCell.className = 'cardInterior';
+					cardCell.setAttribute('id', (row*4+cell));
+					cardCell.className = 'face-down';
+					cardCell.innerHTML = cardCell.id;
+					cardCell.onclick = function(){
+						if (game.faceupWhere()===false){
+							this.className = 'face-up';
+						};
+						game.lift(Number(this.id));
+						for (cards = 0; cards <(game.size()); cards++){
+							if (game.faceupWhere() !== cards){
+								document.getElementById(cards).className = 'face-down'
+							}
+						};
+						for (cards = 0; cards<(game.size()); cards++){
+							if (game.remaining().indexOf(cards) < 0){
+								document.getElementById(cards).className = 'hidden'
+							}
+						}
+						
+					};
+//					cardCell.setAttribute('onclick', 'console.log(' + '\'' + game.lift(Number(cardCell.id)) + '\'' + ')');					
 					gameRow.appendChild(cardCell);
 				}
 				playmat.appendChild(gameRow);
@@ -69,8 +88,22 @@ var MemoryGUI = (function () {
 })();
 
 function doStuff(){
+	console.log('this is before g1u is defined for faceup')
+	console.log(game1.faceupValue());
 	g1u = new MemoryGUI('memorygame', game1);
 	g1u.render();
 }
 
 window.onload = doStuff;
+
+
+/* Testing Consoles:
+
+
+		console.log('this is game lift: ')
+		console.log(game.lift(0));
+		console.log('Is container a string?' + (typeof container === 'string'));
+		console.log('this is your container' + container);
+		console.log('this is your getElementById: ' + document.getElementById(container))
+
+*/
