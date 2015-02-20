1de36e4b778208ac1cc089f79fcb4fba36e1eb34
AntonEmery <antonemery@gmail.com>
2015-02-19 20:43:01 -0800

    2a

--- AntonEmery/memory-gui.js
+++ AntonEmery/memory-gui.js
@@ -23,7 +23,8 @@ var MemoryGUI = (function () {
 
 	function MemoryGUI(container, game) {
 		//render game inside id memorygame in the html
-		//
+		var game = new memorygame; //who knows?
+		game.game.size();
 	}
 
 	return GuiCtor;
