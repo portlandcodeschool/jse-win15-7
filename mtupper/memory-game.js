9362e1c26d7de913d8260dab4480a8bc6a6b5527
mtupper <michael.tupper@gmail.com>
2015-02-22 16:52:12 -0800

    game constructor built

---
+++ mtupper/memory-game.js
@@ -0,0 +1,97 @@
+var MemoryGame = (function() {
+
+	function MemoryGame(cardset) {
+        var slots,
+            length,
+            there;
+
+		// some of the instance methods may need to be
+		// specific to each instance
+		// ...
+
+		this.reset = function() {
+            slots = cardset.values();
+            length = slots.length;
+            there = false;
+            shuffle(slots);
+        };
+		this.remainsAt = function(where) { //boolean
+            return slots[where] !== undefined;
+        };
+        this.valueAt = function(where) {
+            return slots[where];
+        };
+        this.removeAt = function(where) {
+            delete slots[where];
+        };
+        this.faceupValue = function() {
+            return valueAt(there);
+        };
+        this.faceupWhere = function() {
+            return there;
+        };
+        this.remaining = function() {
+            return Object.keys(slots).map(Number);
+        };
+		this.lift = function(here) {
+            if (!isValid(here, length)) return false;
+            if (!remainsAt(here)) return false;
+            if (there === here) return false;
+
+            // must be a face-down card here; proceed...
+            var valHere = valueAt(there);
+            if (there === false) {
+                // no current face-up
+                there = here; // turn here face-up
+            }
+            else {
+                // check for match with face-up
+                if (cardset.match(valHere, valueAt(there))) {
+                    // match; remove both
+                    removeAt(here);
+                    removeAt(there);
+                    // testing report match to console
+                    console.log("match!");
+                }
+                // either way, turn face-up to face-down
+                there = false;
+            }
+            return cardset.display(valHere);
+        };
+
+		// New methods:
+		this.gui = function(useGui) {
+            if (useGui === undefined) return null;
+            return useGui;
+        };
+
+		this.size = function() {
+            return length;
+        }
+	}
+
+	// some of those instance methods could instead be shared
+	// by installing them in GameCtor.prototype
+	// ...
+
+    MemoryGame.prototype.isValid = function(where, length) {
+        return (typeof where === 'number') &&
+            (where % 1 === 0) &&
+            (where > 0) &&
+            (where < length)
+    };
+    MemoryGame.prototype.shuffle = function(array) {
+        // Knuth-Fisher-Yates, modified from http://bost.ocks.org/mike/shuffle/
+        var end = array.length, temp, i;
+            // While there remains elements to shuffle
+        while (end > 1) {
+            // Pick remaining element
+            i = Math.floor(Math.random() * end--);
+            // And swap it with the current element
+            temp = array[end];
+            array[end] = array[i];
+            array[i] = temp;
+        }
+    };
+	return MemoryGame;
+})();
