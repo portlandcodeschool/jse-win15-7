28ef8d20d693e861261a7a67d83c1881457e1fba
Greg Borchers <greg.borchers@comcast.net>
2015-03-03 00:44:46 -0800

    working except for shuffle on reset - but it is stunningly ugly

--- gregBorchers/template/HW7memory-gui.js
+++ gregBorchers/template/HW7memory-gui.js
@@ -1,40 +1,60 @@
 var MemoryGUI = (function() {
 
-   
+
 
     function GuiCtor(container, game) {
 
         // public instance methods:
         this.reset = function() {
-            //...
+            console.log('******* reset BUTTON PRESSED');
+            window.go();
+            game.reset();
         };
+
         this.show = function(where, what) {
-            //...
-        };
-        this.removeSoon = function(whereArr) {
-            //...
-        };
-        this.hideSoon = function(whereArr) {
-            //...
+            $(where).removeClass('facedown')
+                .addClass('faceup');
         };
 
+        this.removeSoon = function(whereAt) {
+            window.setTimeout(function() {
+                var elem = document.getElementById(whereAt);
+                elem.classList.remove('faceup');
+                elem.classList.add('gone');
+            }, 750);          
+        };
 
-        // Do some initial setup and rendering...
+        this.hideSoon = function(whereAt) {
+            window.setTimeout(function() {
+                var elem = document.getElementById(whereAt);
+                elem.classList.remove('faceup');
+                elem.classList.add('facedown');
+            }, 750);    
+        };
+
+        this.wait = function() {
+            //does nothing but filled the need for a function to wait on;
+        };
 
         this.setupCallbacks = function(cards) {
-            var board = new CardBoard('memorygame',8);
+            var board = new CardBoard('memorygame', 8);
             board.render();
-            board.populate(this.onClickHandler,cards);
+            board.populate(this.onClickHandler, cards);
+            // setup the reset button
+            $('button').click(this.reset); //TODO get a better button name :-)
         };
 
-// STATUS - only logging for now.
-        this.onClickHandler = function(event) { // onclick gets an event object as the parameter
-           
-            console.log("\njust clicked on ID = " + this.id);
-            // TIP - use this tactic for investigations --- > window.lastEvent = event;
-            console.log("\njust clicked on ID = x=" + event.clientX + " y=" + event.clientY);
 
-            console.log('now we need to CALL the LIFT(' + this.id + ') function');
+        this.onClickHandler = function(event) { // onclick gets an event object as the parameter
+
+            console.log("\njust clicked on ID = " + this.id + 'which is a ' + $(event.target).text());
+
+            var liftResult = game.lift(Number(this.id));
+            console.log('game.lift(Number(this.id)) returned*** ' + liftResult);
+            if (liftResult === undefined) {
+                liftResult = false; // HACK ToFIX based on lift logic, this works for now on first click
+            }
+            gui.show(event.target, liftResult);
 
         };
     } // end GUI Ctor
@@ -42,15 +62,15 @@ var MemoryGUI = (function() {
     return GuiCtor;
 })();
 
-//  HACK starts here.... :-)
+//  WORLD'S UGLIEST BOARD HACK starts here.... :-)
 //
 // TEMP solution, make the checkboard the card game
 // shrunk down distant cousin to the checkerboard renamed the "CardBoard"
 
 var CardBoard = (function() {
-    var CardBoard = function(container,size) {
+    var CardBoard = function(container, size) {
         var maxCol = 8;
-        this.numRows = Math.ceil(maxCol/size);
+        this.numRows = Math.ceil(maxCol / size);
         this.numCols = maxCol;
         this.populate = populate;
         this.render = render;
@@ -60,19 +80,19 @@ var CardBoard = (function() {
         }
     };
 
-    this.populate = function(eventHandlerFn, items) {  // hint: items are cards
+    this.populate = function(eventHandlerFn, items) { // hint: items are cards
         var displayList = items.values();
-        
+
         // TODO replace this with 100% JQuery No javascript...
         for (var id = 0, r = 0; r < this.numRows; ++r) { // row loop
             for (var c = 0; c < this.numCols; ++c, ++id) { // col loop
-                
-                    // using ID, get the current cell and stuff it with the item to display 
-                    var cell = document.getElementById(id);                  
-                    var displayText = document.createTextNode(items.display(displayList[id]));
-                    cell.appendChild(displayText);
-                    // attach the handler logOnClick
-                    cell.addEventListener('click', eventHandlerFn);       
+
+                // using ID, get the current cell and stuff it with the item to display 
+                var cell = document.getElementById(id);
+                var displayText = document.createTextNode(items.display(displayList[id]));
+                cell.appendChild(displayText);
+                console.log('populate() appended ID:' + id + ' with ' + items.display(displayList[id])); // attach the handler logOnClick
+                cell.addEventListener('click', eventHandlerFn);
             }
         }
     };
@@ -83,18 +103,28 @@ var CardBoard = (function() {
         // TODO replace this with 100% JQuery No javascript...
         var table = document.createElement('table');
 
-        for (var r = 0, i = 0; r < this.numRows; ++r) {
+        for (var r = 0, id = 0; r < this.numRows; ++r) {
             var tr = document.createElement('tr');
             table.appendChild(tr);
-            for (var c = 0; c < this.numCols; ++c, ++i) {
+            for (var c = 0; c < this.numCols; ++c, ++id) {
                 var td = document.createElement('td');
-                td.setAttribute('id', i);
-                td.classList.add(((r + c) % 2) ? 'faceup' : 'faceup');
+                td.setAttribute('id', parseInt(id));
+                td.classList.add('facedown');
                 tr.appendChild(td);
 
             }
         }
+
+        // setup the reset button
+
+        var btn = document.createElement('button');
+        var text = document.createTextNode('Reset');
+        btn.appendChild(text);
+
+
         this.board.appendChild(table);
+        this.board.appendChild(btn);
+
     };
 
     return CardBoard;
--- gregBorchers/template/HW7memory-main.js
+++ gregBorchers/template/HW7memory-main.js
@@ -5,12 +5,12 @@ function go() {
 	cards = new MemoryCards();//<--maybe add argument for some card sets
 	game  = new MemoryGame(cards);
 	gui   = new MemoryGUI('memorygame',game);
-	//game.gui(gui); // link game to gui
-	var outStr = '';
-	for (var i = 0; i < 8; i++) {
-		outStr += game.lift(i) + '\n';		
-	}
-	console.log("test loop of cards " + outStr);
+	// //game.gui(gui); // link game to gui
+	// var outStr = '';
+	// for (var i = 0; i < 8; i++) {
+	// 	outStr += game.lift(i) + '\n';		
+	// }
+	//console.log("test loop of cards " + outStr);
 	console.log("seting up callbacks");
 	gui.setupCallbacks(cards);
 }
